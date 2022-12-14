21fabac559d87687b1db249da86a0d30ebc64c6c
HemanthPaila <pailahemanth2002@gmail.com>
2022-12-14 17:16:42 +0530

    did a first component but not working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "car insurence",
+      amount: "100",
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "new tv",
+      amount: "15000",
+      date: new Date(2021, 7, 14),
+    },
+    {
+      id: "e3",
+      title: "cycle",
+      amount: "5000",
+      date: new Date(2022, 7, 14),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
