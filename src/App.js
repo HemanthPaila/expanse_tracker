6e3da56a27ab202ad1432954c9b9dd32cea6d91a
HemanthPaila <pailahemanth2002@gmail.com>
2022-12-15 16:43:07 +0530

    modified app.js and Expenses.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from "react";
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "car insurence",
+    amount: "100",
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "new tv",
+    amount: "15000",
+    date: new Date(2021, 7, 14),
+  },
+  {
+    id: "e3",
+    title: "cycle",
+    amount: "5000",
+    date: new Date(2022, 7, 14),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "car insurence",
-      amount: "100",
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "new tv",
-      amount: "15000",
-      date: new Date(2021, 7, 14),
-    },
-    {
-      id: "e3",
-      title: "cycle",
-      amount: "5000",
-      date: new Date(2022, 7, 14),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const saveExpenseDataHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
