22baeb8ea300e392b659a58825c67a79b159cd07
HemanthPaila <pailahemanth2002@gmail.com>
2023-01-16 08:31:46 +0530

    modified newexpenses compenent

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+const Chart = (props) => {
+  <div className="chart"></div>;
+};
+export default Chart;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,16 +1,33 @@
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new expenses</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
